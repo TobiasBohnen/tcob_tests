7b17a44a3a0a1b0136961d26abca8dd699a91df8
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2024-09-27 06:52:04 +0200

    fixed test

--- RayTests.cpp
+++ RayTests.cpp
@@ -5,12 +5,12 @@ TEST_CASE("GFX.Ray.Rect")
     {
         rect_f rect {10, 0, 20, 20};
         ray    ray {{5, 5}, degree_f {90}};
-        REQUIRE(ray.intersect_rect(rect) == std::vector<point_f> {{30, 5}, {10, 5}});
+        REQUIRE(ray.intersect_rect(rect) == std::vector<ray::result> {{{30, 5}, 25}, {{10, 5}, 5}});
     }
     {
         rect_f rect {0, 0, 50, 50};
         ray    ray {{60, 30}, degree_f {270}};
-        REQUIRE(ray.intersect_rect(rect) == std::vector<point_f> {{50, 30}, {0, 30}});
+        REQUIRE(ray.intersect_rect(rect) == std::vector<ray::result> {{{50, 30}, 10}, {{0, 30}, 60}});
     }
 }
 
@@ -18,7 +18,7 @@ TEST_CASE("GFX.Ray.Circle")
 {
     {
         ray ray {{0, 10}, degree_f {90}};
-        REQUIRE(ray.intersect_circle({10, 10}, 4) == std::vector<point_f> {{6, 10}, {14, 10}});
+        REQUIRE(ray.intersect_circle({10, 10}, 4) == std::vector<ray::result> {{{6, 10}, 6}, {{14, 10}, 14}});
     }
 }
 
@@ -29,6 +29,6 @@ TEST_CASE("GFX.Ray.Func")
         func.StartValue = {2, 2};
         func.EndValue   = {12, 12};
         ray ray {{1, 5}, degree_f {90}};
-        REQUIRE(ray.intersect_function(func, 0.01) == std::vector<point_f> {{5, 5}});
+        REQUIRE(ray.intersect_function(func, 0.01) == std::vector<ray::result> {{{5, 5}, 4}});
     }
 }
