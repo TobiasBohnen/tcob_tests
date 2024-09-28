edd1a8ead6c72d48e6ee2cc320c545a83793be3d
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2024-09-28 12:17:37 +0200

    api update

--- RayTests.cpp
+++ RayTests.cpp
@@ -4,12 +4,12 @@ TEST_CASE("GFX.Ray.Rect")
 {
     {
         rect_f rect {10, 0, 20, 20};
-        ray    ray {{5, 5}, degree_f {90}};
+        ray    ray {{{5, 5}, degree_f {90}}};
         REQUIRE(ray.intersect_rect(rect) == std::vector<ray::result> {{{30, 5}, 25}, {{10, 5}, 5}});
     }
     {
         rect_f rect {0, 0, 50, 50};
-        ray    ray {{60, 30}, degree_f {270}};
+        ray    ray {{{60, 30}, degree_f {270}}};
         REQUIRE(ray.intersect_rect(rect) == std::vector<ray::result> {{{50, 30}, 10}, {{0, 30}, 60}});
     }
 }
@@ -17,7 +17,7 @@ TEST_CASE("GFX.Ray.Rect")
 TEST_CASE("GFX.Ray.Circle")
 {
     {
-        ray ray {{0, 10}, degree_f {90}};
+        ray ray {{{0, 10}, degree_f {90}}};
         REQUIRE(ray.intersect_circle({10, 10}, 4) == std::vector<ray::result> {{{6, 10}, 6}, {{14, 10}, 14}});
     }
 }
@@ -28,7 +28,7 @@ TEST_CASE("GFX.Ray.Func")
         tweening::func::linear<point_f> func;
         func.StartValue = {2, 2};
         func.EndValue   = {12, 12};
-        ray ray {{1, 5}, degree_f {90}};
+        ray ray {{{1, 5}, degree_f {90}}};
         REQUIRE(ray.intersect_function(func, 0.01) == std::vector<ray::result> {{{5, 5}, 4}});
     }
 }
