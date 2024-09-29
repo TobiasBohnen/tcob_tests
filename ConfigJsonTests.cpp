af3c77598bf5eada937883cd41b009e62a16c087
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2024-09-29 12:38:24 +0200

    fixed test

--- ConfigJsonTests.cpp
+++ ConfigJsonTests.cpp
@@ -245,7 +245,8 @@ TEST_CASE("Data.Json.TcobTypes")
 TEST_CASE("Data.Json.TestSuite")
 {
     auto const files(io::enumerate("testfiles/json/", {.String = "*" + EXT}, false));
-    REQUIRE(files.size() == 232);
+    REQUIRE(files.size() == 233);
+
     for (auto const& file : files) {
         object     obj;
         auto const objStatus {obj.load(file, true) == load_status::Ok};
