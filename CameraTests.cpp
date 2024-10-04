d8e54be6565fd7e822d651d6ab961d4ba534f183
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2024-10-04 16:08:55 +0200

    api update

--- CameraTests.cpp
+++ CameraTests.cpp
@@ -2,40 +2,40 @@
 
 TEST_CASE("GFX.Camera.Frustum")
 {
-    SUBCASE("box")
+    SUBCASE("size")
     {
         gfx::camera camera;
         REQUIRE(camera.get_transformed_viewport() == rect_f {0, 0, 0, 0});
 
-        camera.set_size({800, 600});
+        camera.Size = {800, 600};
         REQUIRE(camera.get_transformed_viewport() == rect_f {0, 0, 800, 600});
     }
     SUBCASE("position")
     {
         gfx::camera camera;
-        camera.set_offset({200, 300});
-        camera.set_position({400, 600});
+        camera.Offset   = {200, 300};
+        camera.Position = {400, 600};
         REQUIRE(camera.get_transformed_viewport() == rect_f {400, 600, 0, 0});
 
-        camera.set_size({800, 600});
+        camera.Size = {800, 600};
         REQUIRE(camera.get_transformed_viewport() == rect_f {400, 600, 800, 600});
     }
     SUBCASE("zoom in")
     {
         gfx::camera camera;
-        camera.set_position({400, 600});
-        camera.set_size({800, 600});
+        camera.Position = {400, 600};
+        camera.Size     = {800, 600};
 
-        camera.set_zoom({2, 4});
+        camera.Zoom = {2, 4};
         REQUIRE(camera.get_transformed_viewport() == rect_f {600, 825, 800 / 2.f, 600 / 4.f});
     }
     SUBCASE("zoom out")
     {
         gfx::camera camera;
-        camera.set_position({400, 600});
-        camera.set_size({800, 600});
+        camera.Position = {400, 600};
+        camera.Size     = {800, 600};
 
-        camera.set_zoom({0.5f, 0.25f});
+        camera.Zoom = {0.5f, 0.25f};
         REQUIRE(camera.get_transformed_viewport() == rect_f {0, -300, 800 * 2, 600 * 4});
     }
 }
