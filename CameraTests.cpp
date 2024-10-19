03824df48775cecd6b8c9e66d79b1a5856a24d36
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2024-10-19 12:26:10 +0200

    api update

--- CameraTests.cpp
+++ CameraTests.cpp
@@ -1,39 +1,58 @@
 #include "tests.hpp"
 
-TEST_CASE("GFX.Camera.Frustum")
+class dummy_render_target : public render_target {
+public:
+    dummy_render_target()
+        : render_target {nullptr}
+    {
+    }
+
+protected:
+    auto get_size() const -> size_i override { return _size; }
+    void set_size(size_i newsize) override { _size = newsize; }
+    void prepare_render(bool) override { }
+
+    size_i _size;
+};
+
+TEST_CASE("GFX.Camera.Viewport")
 {
     SUBCASE("size")
     {
-        gfx::camera camera;
+        dummy_render_target tex;
+        gfx::camera&        camera {tex.get_camera()};
         REQUIRE(camera.get_transformed_viewport() == rect_f {0, 0, 0, 0});
 
-        camera.Size = {800, 600};
+        tex.Size = {800, 600};
         REQUIRE(camera.get_transformed_viewport() == rect_f {0, 0, 800, 600});
     }
     SUBCASE("position")
     {
-        gfx::camera camera;
-        camera.Offset   = {200, 300};
-        camera.Position = {400, 600};
+        dummy_render_target tex;
+        gfx::camera&        camera {tex.get_camera()};
+        camera.ViewOffset = {200, 300};
+        camera.Position   = {400, 600};
         REQUIRE(camera.get_transformed_viewport() == rect_f {400, 600, 0, 0});
 
-        camera.Size = {800, 600};
+        tex.Size = {800, 600};
         REQUIRE(camera.get_transformed_viewport() == rect_f {400, 600, 800, 600});
     }
     SUBCASE("zoom in")
     {
-        gfx::camera camera;
+        dummy_render_target tex;
+        gfx::camera&        camera {tex.get_camera()};
         camera.Position = {400, 600};
-        camera.Size     = {800, 600};
+        tex.Size        = {800, 600};
 
         camera.Zoom = {2, 4};
         REQUIRE(camera.get_transformed_viewport() == rect_f {600, 825, 800 / 2.f, 600 / 4.f});
     }
     SUBCASE("zoom out")
     {
-        gfx::camera camera;
+        dummy_render_target tex;
+        gfx::camera&        camera {tex.get_camera()};
         camera.Position = {400, 600};
-        camera.Size     = {800, 600};
+        tex.Size        = {800, 600};
 
         camera.Zoom = {0.5f, 0.25f};
         REQUIRE(camera.get_transformed_viewport() == rect_f {0, -300, 800 * 2, 600 * 4});
