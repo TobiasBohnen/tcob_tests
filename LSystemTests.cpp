38949d790df85a0d16e6215b854bfe9621682de6
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2025-02-25 20:18:56 +0100

    api update

--- LSystemTests.cpp
+++ LSystemTests.cpp
@@ -9,15 +9,17 @@ TEST_CASE("GFX.LSystem.Algae")
     rules  : (A → AB), (B → A)
     */
 
-    l_system sys {"A", {{'A', "AB"}, {'B', "A"}}};
-    REQUIRE(sys.generate(0) == "A");
-    REQUIRE(sys.generate(1) == "AB");
-    REQUIRE(sys.generate(2) == "ABA");
-    REQUIRE(sys.generate(3) == "ABAAB");
-    REQUIRE(sys.generate(4) == "ABAABABA");
-    REQUIRE(sys.generate(5) == "ABAABABAABAAB");
-    REQUIRE(sys.generate(6) == "ABAABABAABAABABAABABA");
-    REQUIRE(sys.generate(7) == "ABAABABAABAABABAABABAABAABABAABAAB");
+    l_system sys {};
+    sys.add_rule('A', {"AB"});
+    sys.add_rule('B', {"A"});
+    REQUIRE(sys.generate("A", 0) == "A");
+    REQUIRE(sys.generate("A", 1) == "AB");
+    REQUIRE(sys.generate("A", 2) == "ABA");
+    REQUIRE(sys.generate("A", 3) == "ABAAB");
+    REQUIRE(sys.generate("A", 4) == "ABAABABA");
+    REQUIRE(sys.generate("A", 5) == "ABAABABAABAAB");
+    REQUIRE(sys.generate("A", 6) == "ABAABABAABAABABAABABA");
+    REQUIRE(sys.generate("A", 7) == "ABAABABAABAABABAABABAABAABABAABAAB");
 }
 
 TEST_CASE("GFX.LSystem.FractalTree")
@@ -29,10 +31,12 @@ TEST_CASE("GFX.LSystem.FractalTree")
     rules  : (1 → 11), (0 → 1[0]0)
     */
 
-    l_system sys {"0", {{'1', "11"}, {'0', "1[0]0"}}};
-    REQUIRE(sys.generate(1) == "1[0]0");
-    REQUIRE(sys.generate(2) == "11[1[0]0]1[0]0");
-    REQUIRE(sys.generate(3) == "1111[11[1[0]0]1[0]0]11[1[0]0]1[0]0");
+    l_system sys {};
+    sys.add_rule('1', {"11"});
+    sys.add_rule('0', {"1[0]0"});
+    REQUIRE(sys.generate("0", 1) == "1[0]0");
+    REQUIRE(sys.generate("0", 2) == "11[1[0]0]1[0]0");
+    REQUIRE(sys.generate("0", 3) == "1111[11[1[0]0]1[0]0]11[1[0]0]1[0]0");
 }
 
 TEST_CASE("GFX.LSystem.KochCurve")
@@ -44,8 +48,9 @@ TEST_CASE("GFX.LSystem.KochCurve")
     rules  : (F → F+F-F-F+F)
     */
 
-    l_system sys {"F", {{'F', "F+F-F-F+F"}}};
-    REQUIRE(sys.generate(1) == "F+F-F-F+F");
-    REQUIRE(sys.generate(2) == "F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F");
-    REQUIRE(sys.generate(3) == "F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F+F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F-F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F-F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F+F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F");
+    l_system sys {};
+    sys.add_rule('F', {"F+F-F-F+F"});
+    REQUIRE(sys.generate("F", 1) == "F+F-F-F+F");
+    REQUIRE(sys.generate("F", 2) == "F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F");
+    REQUIRE(sys.generate("F", 3) == "F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F+F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F-F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F-F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F+F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F");
 }
