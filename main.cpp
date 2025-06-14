659e35c288033ef40eb5b2e8ae40ed17c9ea22b2
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2025-06-14 23:41:00 +0200

    api update

--- main.cpp
+++ main.cpp
@@ -5,15 +5,13 @@
 
 auto main(int argc, char** argv) -> int
 {
-    auto pl{tcob::platform::HeadlessInit(argv[0], "tcob_tests.log")};
+    auto pl {tcob::platform::HeadlessInit("tcob_tests.log")};
 
     doctest::Context context;
     context.applyCommandLine(argc, argv);
-    int res = context.run(); // run
+    int const res {context.run()}; // run
 
-    if (context.shouldExit()) {
-        return res;
-    }
+    if (context.shouldExit()) { return res; }
 
     return res;
 }
