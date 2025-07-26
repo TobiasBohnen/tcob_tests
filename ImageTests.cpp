475863fab85884ac8fe6f3b29450e51c9acc546f
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2025-07-26 15:43:26 +0200

    add GFX.Image.RemoveAlpha

--- ImageTests.cpp
+++ ImageTests.cpp
@@ -281,3 +281,25 @@ TEST_CASE("GFX.Image.DataRect")
     std::vector<u8> data6 {image.data({0, 2, 5, 1})};
     REQUIRE(data6 == target6);
 }
+
+TEST_CASE("GFX.Image.RemoveAlpha")
+{
+
+    u32 const                                           channels {4};
+    constexpr size_i                                    size {4, 4};
+    std::array<u8, size.Width * size.Height * channels> source {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63};
+    std::vector<u8> target {
+        0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14,
+        16, 17, 18, 20, 21, 22, 24, 25, 26, 28, 29, 30,
+        32, 33, 34, 36, 37, 38, 40, 41, 42, 44, 45, 46,
+        48, 49, 50, 52, 53, 54, 56, 57, 58, 60, 61, 62};
+
+    auto            image {image::Create(size, image::format::RGBA, source)};
+    auto            newImage {remove_alpha {}(image)};
+    std::vector<u8> data1 {newImage.data().begin(), newImage.data().end()};
+    REQUIRE(data1 == target);
+}
