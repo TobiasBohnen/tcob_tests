aec0b3fd3e174b04568a268c1aa24c0dd02d4f32
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2025-12-27 21:48:16 +0100

    api update

--- SignalTests.cpp
+++ SignalTests.cpp
@@ -66,6 +66,33 @@ TEST_CASE("Core.Signal.Connect")
             REQUIRE(value == ExpValue * 10);
         }
     }
+
+    SUBCASE("Stateful lambda")
+    {
+        struct Event {
+            i32 Value {0};
+        };
+
+        signal<Event> sig0;
+        i32           result {0};
+
+        sig0.connect([&result, counter = 0](Event& ev) mutable {
+            counter++;
+            result = ev.Value * counter;
+        });
+
+        Event ev1 {10};
+        sig0(ev1);
+        REQUIRE(result == 10); // 10 * 1
+
+        Event ev2 {10};
+        sig0(ev2);
+        REQUIRE(result == 20); // 10 * 2
+
+        Event ev3 {10};
+        sig0(ev3);
+        REQUIRE(result == 30); // 10 * 3
+    }
 }
 
 TEST_CASE("Core.Signal.Disconnect")
@@ -175,5 +202,79 @@ TEST_CASE("Core.Signal.ChangeEvArgs")
         REQUIRE(ev.Value == 0);
         sig0(ev);
         REQUIRE(ev.Value == 100);
+    }
+}
+
+TEST_CASE("Core.Signal.NestedSubscription")
+{
+    SUBCASE("Connect during emission")
+    {
+        signal<> sig0;
+        i32      callCount {0};
+
+        sig0.connect([&sig0, &callCount] {
+            callCount++;
+            sig0.connect([&callCount] {
+                callCount += 10;
+            });
+        });
+
+        sig0.connect([&callCount] {
+            callCount += 100;
+        });
+
+        sig0();
+        REQUIRE(callCount > 0);
+        REQUIRE(sig0.slot_count() == 3);
+    }
+
+    SUBCASE("Disconnect during emission")
+    {
+        signal<>          sig0;
+        i32               callCount {0};
+        scoped_connection c2;
+
+        sig0.connect([&c2, &callCount] {
+            callCount++;
+            c2.disconnect();
+        });
+
+        c2 = sig0.connect([&callCount] {
+            callCount += 10;
+        });
+
+        sig0.connect([&callCount] {
+            callCount += 100;
+        });
+
+        sig0();
+        callCount = 0;
+        sig0();
+        REQUIRE(callCount == 101);
+    }
+
+    SUBCASE("Recursive signal emission")
+    {
+        signal<> sig0;
+        i32      callCount {0};
+        i32      maxDepth {0};
+        i32      currentDepth {0};
+
+        sig0.connect([&sig0, &callCount, &currentDepth, &maxDepth] {
+            callCount++;
+            currentDepth++;
+            maxDepth = std::max(maxDepth, currentDepth);
+
+            if (currentDepth < 3) {
+                sig0();
+            }
+
+            currentDepth--;
+        });
+
+        sig0();
+
+        REQUIRE(callCount == 3);
+        REQUIRE(maxDepth == 3);
     }
 }
