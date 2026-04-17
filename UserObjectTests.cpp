220df9010b27b835bc4dba9f7263f97d8116b4c8
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2026-04-17 21:19:35 +0200

    update

--- UserObjectTests.cpp
+++ UserObjectTests.cpp
@@ -5,19 +5,17 @@ TEST_CASE("Core.UserObject.Construction")
     SUBCASE("default state")
     {
         user_object obj;
-
         REQUIRE(obj.has_value() == false);
         REQUIRE(obj.get<i32>() == nullptr);
+        REQUIRE(obj.type() == typeid(void));
     }
 
     SUBCASE("value construction (i32)")
     {
         user_object obj {42};
-
         REQUIRE(obj.has_value());
         REQUIRE(obj.type() == typeid(i32));
-
-        auto* v = obj.get<i32>();
+        auto* v {obj.get<i32>()};
         REQUIRE(v != nullptr);
         REQUIRE(*v == 42);
     }
@@ -25,34 +23,28 @@ TEST_CASE("Core.UserObject.Construction")
     SUBCASE("value construction (string)")
     {
         user_object obj {string("hello")};
-
         REQUIRE(obj.type() == typeid(string));
-
-        auto* v = obj.get<string>();
+        auto* v {obj.get<string>()};
         REQUIRE(v != nullptr);
         REQUIRE(*v == "hello");
     }
 
-    SUBCASE("Make<T> construction")
+    SUBCASE("shared_ptr construction")
     {
-        struct Foo {
-            i32 a;
-            i32 b;
-            Foo(i32 x, i32 y)
-                : a(x)
-                , b(y)
-            {
-            }
-        };
+        auto        ptr {std::make_shared<i32>(99)};
+        user_object obj {ptr};
+        REQUIRE(obj.has_value());
+        REQUIRE(obj.type() == typeid(i32));
+        auto* v {obj.get<i32>()};
+        REQUIRE(v != nullptr);
+        REQUIRE(*v == 99);
+    }
 
-        auto obj {user_object::Make<Foo>(1, 2)};
-
-        REQUIRE(obj.type() == typeid(Foo));
-
-        auto* f = obj.get<Foo>();
-        REQUIRE(f != nullptr);
-        REQUIRE(f->a == 1);
-        REQUIRE(f->b == 2);
+    SUBCASE("shared_ptr construction shares ownership")
+    {
+        auto        ptr {std::make_shared<i32>(42)};
+        user_object obj {ptr};
+        REQUIRE(ptr.use_count() == 2);
     }
 }
 
@@ -62,7 +54,6 @@ TEST_CASE("Core.UserObject.Assignment")
     {
         user_object obj;
         obj = 10;
-
         REQUIRE(obj.type() == typeid(i32));
         REQUIRE(*obj.get<i32>() == 10);
     }
@@ -71,23 +62,38 @@ TEST_CASE("Core.UserObject.Assignment")
     {
         user_object obj;
         obj = string("world");
-
         REQUIRE(obj.type() == typeid(string));
         REQUIRE(*obj.get<string>() == "world");
+    }
+
+    SUBCASE("assign shared_ptr")
+    {
+        auto        ptr {std::make_shared<i32>(55)};
+        user_object obj;
+        obj = ptr;
+        REQUIRE(obj.type() == typeid(i32));
+        REQUIRE(*obj.get<i32>() == 55);
+        REQUIRE(ptr.use_count() == 2);
     }
 
     SUBCASE("overwrite changes type")
     {
         user_object obj;
-
         obj = 1;
         REQUIRE(obj.type() == typeid(i32));
-
         obj = string("now string");
-
         REQUIRE(obj.type() == typeid(string));
         REQUIRE(obj.get<i32>() == nullptr);
         REQUIRE(*obj.get<string>() == "now string");
+    }
+
+    SUBCASE("overwrite shared_ptr releases previous")
+    {
+        auto        ptr {std::make_shared<i32>(1)};
+        user_object obj {ptr};
+        REQUIRE(ptr.use_count() == 2);
+        obj = string("replaced");
+        REQUIRE(ptr.use_count() == 1);
     }
 }
 
@@ -102,7 +108,6 @@ TEST_CASE("Core.UserObject.TypeAndAccess")
     SUBCASE("get returns nullptr on mismatch")
     {
         user_object obj {100};
-
         REQUIRE(obj.get<f64>() == nullptr);
         REQUIRE(obj.get<string>() == nullptr);
     }
@@ -110,24 +115,38 @@ TEST_CASE("Core.UserObject.TypeAndAccess")
     SUBCASE("reset clears state")
     {
         user_object obj {string("test")};
-
         REQUIRE(obj.has_value());
-
         obj.reset();
-
         REQUIRE(!obj.has_value());
         REQUIRE(obj.type() == typeid(void));
         REQUIRE(obj.get<string>() == nullptr);
     }
 
+    SUBCASE("reset releases shared_ptr")
+    {
+        auto        ptr {std::make_shared<i32>(1)};
+        user_object obj {ptr};
+        REQUIRE(ptr.use_count() == 2);
+        obj.reset();
+        REQUIRE(ptr.use_count() == 1);
+    }
+
     SUBCASE("has_value behavior")
     {
         user_object obj;
-
         REQUIRE(!obj.has_value());
-
         obj = 123;
-
         REQUIRE(obj.has_value());
+        obj.reset();
+        REQUIRE(!obj.has_value());
+    }
+
+    SUBCASE("copy shares ownership")
+    {
+        auto        ptr {std::make_shared<i32>(7)};
+        user_object a {ptr};
+        user_object b {a}; // NOLINT
+        REQUIRE(ptr.use_count() == 3);
+        REQUIRE(*b.get<i32>() == 7);
     }
 }
