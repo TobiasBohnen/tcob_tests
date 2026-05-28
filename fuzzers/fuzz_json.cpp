0f6debbf76827b36541dcd1716a2c08f466f258b
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2026-05-28 19:52:08 +0200

    add json fuzzer

---
+++ fuzzers/fuzz_json.cpp
@@ -0,0 +1,31 @@
+
+
+#include <cstddef>
+#include <cstdint>
+#include <span>
+#include <tuple>
+
+#include <tcob/tcob.hpp>
+
+using namespace tcob;
+static std::shared_ptr<tcob::platform> pl;
+
+extern "C" auto LLVMFuzzerInitialize(int* /* argc */, char*** /* argv */) -> int
+{
+    pl = tcob::platform::HeadlessInit("tcob_tests.log");
+    return 0;
+}
+
+extern "C" auto LLVMFuzzerTestOneInput(uint8_t const* data, size_t size) -> int
+{
+    try {
+        io::isstream stream {std::span<std::byte const> {reinterpret_cast<std::byte const*>(data), size}};
+        stream.seek(0, io::seek_dir::Begin);
+        data::object obj;
+        std::ignore = obj.load(stream, ".json");
+    } catch (...) {
+        // Never allow exceptions to escape
+    }
+
+    return 0;
+}
